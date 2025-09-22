d556301385fa80a79e0873cab89b2fae6fcd76c7
Fledge Shiu <xzk0701@gmail.com>
2025-09-22 14:44:01 +0800

    Format code and fix lint issue.

--- frontend/src/routes/__root.tsx
+++ frontend/src/routes/__root.tsx
@@ -1,9 +1,9 @@
 import { createRootRoute, Outlet } from "@tanstack/react-router";
 
 const RootLayout = () => (
-    <div className="h-dvh w-dvw">
-        <Outlet />
-    </div>
+  <div className="h-dvh w-dvw">
+    <Outlet />
+  </div>
 );
 
 export const Route = createRootRoute({ component: RootLayout });
--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -2,13 +2,13 @@ import { createFileRoute } from "@tanstack/react-router";
 import { Button } from "@/components/ui/button";
 
 export const Route = createFileRoute("/")({
-    component: Index,
+  component: Index,
 });
 
 function Index() {
-    return (
-        <div className="flex min-h-svh flex-col items-center justify-center">
-            <Button>Click me</Button>
-        </div>
-    );
+  return (
+    <div className="flex min-h-svh flex-col items-center justify-center">
+      <Button>Click me</Button>
+    </div>
+  );
 }
