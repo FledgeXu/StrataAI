1182508c464499ac37f2c9514ab0779d98d1ca82
Fledge Shiu <xzk0701@gmail.com>
2025-09-23 21:45:08 +0800

    Adjust auth endpoint.

--- backend/src/strata_ai/api/v1/auth.py
+++ backend/src/strata_ai/api/v1/auth.py
@@ -5,14 +5,10 @@ from strata_ai.schemas.user import UserCreate, UserRead
 
 router = APIRouter(prefix="/auth", tags=["auth"])
 
-router.include_router(fastapi_users.get_auth_router(auth_backend), prefix="/jwt")
+router.include_router(fastapi_users.get_auth_router(auth_backend))
 
-router.include_router(
-    fastapi_users.get_register_router(UserRead, UserCreate), prefix="/register"
-)
+router.include_router(fastapi_users.get_register_router(UserRead, UserCreate))
 
-router.include_router(fastapi_users.get_verify_router(UserRead), prefix="/verify")
+router.include_router(fastapi_users.get_verify_router(UserRead))
 
-router.include_router(
-    fastapi_users.get_reset_password_router(), prefix="/reset-password"
-)
+router.include_router(fastapi_users.get_reset_password_router())
