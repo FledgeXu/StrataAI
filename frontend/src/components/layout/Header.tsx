1fb3cc13a491fa9428cc939a48dab9e6c5bb0f16
Fledge Shiu <xzk0701@gmail.com>
2025-09-25 22:47:03 +0800

    Add basic Header.

--- frontend/src/components/layout/Header.tsx
+++ frontend/src/components/layout/Header.tsx
@@ -1,15 +1,20 @@
 import { PanelLeft, User } from "lucide-react";
 import { Separator } from "@/components/ui/separator";
+import { Button } from "@/components/ui/button";
 
 export function Header() {
     return (
-        <div className="bg-amber-100 w-full h-12 flex justify-between items-center-safe p-2">
-            <div className="h-full w-fit flex items-center-safe space-x-2">
-                <PanelLeft className="col-span-1 col-start-1" />
-                <Separator orientation="vertical" className="col-span-1 col-start-2" />
-                <div className="col-span-1 col-start-3">Strata AI</div>
+        <div className="w-full h-14 flex justify-between items-center p-4">
+            <div className="h-full w-fit flex items-center space-x-1">
+                <Button variant={"ghost"}>
+                    <PanelLeft />
+                </Button>
+                <Separator orientation="vertical" />
+                <div className="pl-2">Strata AI</div>
             </div>
-            <User />
+            <Button variant={"ghost"}>
+                <User />
+            </Button>
         </div>
     );
 }
