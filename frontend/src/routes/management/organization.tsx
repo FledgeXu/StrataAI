53002810e67d2c92c4b820d7c30d7cab12906530
Fledge Shiu <xzk0701@gmail.com>
2025-10-03 18:23:34 +0800

    Add basic DataTable and organization management.

--- frontend/src/routes/management/organization.tsx
+++ frontend/src/routes/management/organization.tsx
@@ -1,9 +1,40 @@
+import { DataTable } from "@/components/features/DataTable";
 import { createFileRoute } from "@tanstack/react-router";
+import { type ColumnDef } from "@tanstack/react-table";
 
 export const Route = createFileRoute("/management/organization")({
   component: RouteComponent,
 });
 
+type Organization = {
+  id: string;
+  name: string;
+  createdAt: number;
+};
+
+const TEST_DATA: Organization[] = [
+  {
+    id: "1",
+    name: "Test Server",
+    createdAt: Date.now(),
+  },
+];
+
+const columns: ColumnDef<Organization>[] = [
+  {
+    accessorKey: "name",
+    header: "Name",
+  },
+];
+
 function RouteComponent() {
-  return <div>Hello "/management/organization"!</div>;
+  return (
+    <div>
+      <div>User management</div>
+      <div>
+        management your team and memebers and their account permissions here.
+      </div>
+      <DataTable columns={columns} data={TEST_DATA} />
+    </div>
+  );
 }
