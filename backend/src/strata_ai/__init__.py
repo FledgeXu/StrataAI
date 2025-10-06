f5c53eb1145913ffcd7b768298458bec76d3815b
Fledge Shiu <xzk0701@gmail.com>
2025-10-06 15:47:34 +0800

    Add response_wrapper.

--- backend/src/strata_ai/__init__.py
+++ backend/src/strata_ai/__init__.py
@@ -1,10 +1,16 @@
 from dotenv import load_dotenv
 from fastapi import APIRouter, FastAPI
+from fastapi.responses import ORJSONResponse
 
 from strata_ai.api.v1 import auth
+from strata_ai.middlewares.response_wrapper import wrap_response
 
 load_dotenv()
-app = FastAPI()
+
+app = FastAPI(default_response_class=ORJSONResponse)
+
+app.middleware("http")(wrap_response)
+
 
 v1_router = APIRouter(prefix="/v1")
 v1_router.include_router(auth.router)
