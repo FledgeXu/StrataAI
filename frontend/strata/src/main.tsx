786f57cbf7a7dbf3dcdd1e10bec25a353dfc9ae4
Fledge Shiu <xzk0701@gmail.com>
2026-03-19 22:00:40 +0800

    Add components.

--- frontend/strata/src/main.tsx
+++ frontend/strata/src/main.tsx
@@ -1,9 +1,10 @@
 import { createRouter, RouterProvider } from "@tanstack/react-router";
 import { StrictMode } from "react";
+import "@/index.css"
 import ReactDOM from "react-dom/client";
 
 // Import the generated route tree
-import { routeTree } from "./routeTree.gen";
+import { routeTree } from "@/routeTree.gen";
 
 // Create a new router instance
 const router = createRouter({ routeTree });
--- frontend/strata/src/routes/__root.tsx
+++ frontend/strata/src/routes/__root.tsx
@@ -1,12 +1,14 @@
 import { createRootRoute, Outlet } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
+import { Button } from "@/components/ui/button";
 
 const RootLayout = () => (
-    <>
+    <div>
         <h1 className="text-3xl font-bold underline">Hello world!</h1>
+        <Button>HHHH</Button>
         <Outlet />
         <TanStackRouterDevtools />
-    </>
+    </div>
 );
 
 export const Route = createRootRoute({ component: RootLayout });
