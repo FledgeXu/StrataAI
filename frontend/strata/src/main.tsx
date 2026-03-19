614e6ebe8ea015772fd1a809b2eaed60f14ce11c
Fledge Shiu <xzk0701@gmail.com>
2026-03-19 22:08:33 +0800

    Add axios and react query.

--- frontend/strata/src/main.tsx
+++ frontend/strata/src/main.tsx
@@ -1,4 +1,5 @@
 import { createRouter, RouterProvider } from "@tanstack/react-router";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { StrictMode } from "react";
 import "@/index.css"
 import ReactDOM from "react-dom/client";
@@ -8,6 +9,7 @@ import { routeTree } from "@/routeTree.gen";
 
 // Create a new router instance
 const router = createRouter({ routeTree });
+const queryClient = new QueryClient();
 
 // Register the router instance for type safety
 declare module "@tanstack/react-router" {
@@ -22,7 +24,9 @@ if (!rootElement.innerHTML) {
   const root = ReactDOM.createRoot(rootElement);
   root.render(
     <StrictMode>
-      <RouterProvider router={router} />
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+      </QueryClientProvider>
     </StrictMode>,
   );
 }
